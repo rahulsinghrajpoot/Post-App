7dcf590198baf50bf3060d449278fecdc4d082e6
rahulsinghrajpoot <01rahulsingh2000@gmail.com>
2025-04-10 17:28:22 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/miniPostProject");
+
+const userSchema = mongoose.Schema({
+    username: String,
+    name: String,
+    age: String,
+    email: String,
+    password: String,
+    posts: [
+        {type: mongoose.Schema.Types.ObjectId, ref: "post"}
+    ]
+});
+
+
+module.exports = mongoose.model('user', userSchema);
+
